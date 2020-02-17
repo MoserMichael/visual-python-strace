d27dc51bcab94644f5211d40f772c26f63cf1de3
Michael Moser <mmoser@redhat.com>
2020-02-17 11:39:16 +0200

    firt commit

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ixe
+
+rm -rf dist || true
+
+python3 setup.py sdist bdist_wheel
+
+python3 -m pip install --user --upgrade twine
+
+twine check dist/*
+
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#python3 -m twine upload --repository-url https://pypi.python.org/pypi/ dist/*
